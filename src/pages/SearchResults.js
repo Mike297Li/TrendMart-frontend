3877793880dbbb533d88c95e0f184d44705f1e43
Sarpreet <ssarpreet.786@gmail.com>
2024-11-02 23:19:03 -0400

    Updated code according to new API response structure

--- src/pages/SearchResults.js
+++ src/pages/SearchResults.js
@@ -6,6 +6,7 @@ import '../styles/SearchResults.css';
 const SearchResults = () => {
     const location = useLocation();
     const navigate = useNavigate();
+    const initialResults = location.state?.results || [];
     const initialQuery = location.state?.query || '';
     const [searchValue, setSearchValue] = useState(initialQuery);
     const [rating, setRating] = useState('');
